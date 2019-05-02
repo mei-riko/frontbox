c2a1ee73c80805e33c2b291999d32c9116540d71
mei-riko <dobrovolska.marina@gmail.com>
2019-05-02 03:30:08 +0300

    check all

--- src/js/app.js
+++ src/js/app.js
@@ -19,4 +19,40 @@ $(document).ready(() => {
             }, 
         });
     }
+
+    var inputs = document.querySelectorAll('.inputfile');
+    Array.prototype.forEach.call(inputs, function(input){
+    var label	 = input.nextElementSibling,
+        labelVal = label.innerHTML;
+    input.addEventListener('change', function(e){
+        var fileName = '';
+        if( this.files && this.files.length > 1 )
+        fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+        else
+        fileName = e.target.value.split( '\\' ).pop();
+            if( fileName )
+        label.querySelector( 'span' ).innerHTML = fileName;
+        else
+        label.innerHTML = labelVal;
+        });
+    });
+
+    $('#service').slick({
+        speed: 300,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: true,
+        dots: false, 
+        infinite: true,
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+    });
+
 });
